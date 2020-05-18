6641a33dfbbc35efd44bf0b2d001c720c1ec974b
Felipe Pinheiro <felipep@telecine.com.br>
2020-05-18 20:24:08 -0300

    feat: Added health check endpoint

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+
+@app.route("/health_check", methods=["GET", "OPTIONS"])
+def health_check():
+    return jsonify({"Message": "Success"}), 200
