30ffa0df8ffb7d720d35788274099f94bd0b4696
Felipe Pinheiro <felipep@telecine.com.br>
2020-05-18 20:39:45 -0300

    feat: Added empty endpoints for register event in a competition and show competition result

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -24,5 +24,15 @@ def show_competition(competition_id):
 
 
 @app.route("/competitions", methods=["GET"])
-def list_competition():
+def list_competitions():
+    return jsonify({"Message": "Success"}), 200
+
+
+@app.route("/competition/<competition_id>/register", methods=["POST"])
+def register_for_competition(competition_id):
+    return jsonify({"Message": "Success"}), 200
+
+
+@app.route("/competition/<competition_id>/results", methods=["POST"])
+def show_competition_results(competition_id):
     return jsonify({"Message": "Success"}), 200
